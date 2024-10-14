739230148dea3d92a1796b8885aea2a923fd8a47
wqewqe12 <1730576833@qq.com>
2024-10-14 20:48:31 +0800

    区域子类查询+区域热门

---
+++ travel-modules/travel-article-server/src/main/java/com/travel/lpz/article/mapper/DestinationMapper.java
@@ -0,0 +1,10 @@
+package com.travel.lpz.user.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.travel.lpz.user.domain.UserInfo;
+
+import javax.print.attribute.standard.Destination;
+
+public interface DestinationMapper extends BaseMapper<Destination> {
+
+}
