17fe4d18eed6c58190aefd1a38c42cb47e6d6713
wqewqe12 <1730576833@qq.com>
2024-10-16 17:51:10 +0800

    攻略，主题，分类crud实现

---
+++ travel-modules/travel-article-server/src/main/java/com/travel/lpz/article/mapper/StrategyThemeMapper.java
@@ -0,0 +1,10 @@
+package com.travel.lpz.article.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.travel.lpz.article.domain.Strategy;
+import com.travel.lpz.article.domain.StrategyTheme;
+
+
+public interface StrategyMapper extends BaseMapper<StrategyTheme> {
+
+}
