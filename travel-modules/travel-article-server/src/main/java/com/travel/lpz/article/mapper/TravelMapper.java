3f8ab9c5a4bd9c14162aec24befcb7234fd6d891
wqewqe12 <1730576833@qq.com>
2024-10-22 23:49:55 +0800

    游记基础crud

---
+++ travel-modules/travel-article-server/src/main/java/com/travel/lpz/article/mapper/TravelMapper.java
@@ -0,0 +1,13 @@
+package com.travel.lpz.data.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.travel.lpz.article.domain.Strategy;
+import com.travel.lpz.article.domain.StrategyRank;
+import com.travel.lpz.article.domain.Travel;
+
+import java.util.List;
+
+
+public interface TravelMapper extends BaseMapper<Travel> {
+
+}
